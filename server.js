41ffa8e3fdadd728b27983b123c2852108e17be1
eswilson <eswilson4@gmail.com>
2018-09-26 23:19:50 -0700

    changed mongo db connection for mongolab

--- server.js
+++ server.js
@@ -25,7 +25,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 // connect to Mongo DB
-mongoose.connect("mongodb://localhost/pathdb", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// mongoose.connect("mongodb://localhost/pathdb", { useNewUrlParser: true });
 
 // handlebars
 var exphbs = require("express-handlebars");
